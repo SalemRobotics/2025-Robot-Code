dbf6b9ca463300cf5fbc9026527630d4b7433b86
frc6324-team-laptop <81477144+frc6324-team-laptop@users.noreply.github.com>
2025-02-01 10:07:10 -0500

    added configure auto builder method(unimplemented)

--- 2025-Robot-Code/src/main/java/frc/robot/subsystems/CommandSwerveDrivetrain.java
+++ 2025-Robot-Code/src/main/java/frc/robot/subsystems/CommandSwerveDrivetrain.java
@@ -184,6 +184,10 @@ public class CommandSwerveDrivetrain extends TunerSwerveDrivetrain implements Su
         }
     }
 
+private void ConfigureAutoBuilder(){
+    
+}
+
     /**
      * Returns a command that applies the specified control request to this swerve drivetrain.
      *
