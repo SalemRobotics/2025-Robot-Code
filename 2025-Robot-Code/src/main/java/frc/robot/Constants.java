d784978805ffcce0a6038a56958ecd76d985362a
demiandk <152564707+demiandk@users.noreply.github.com>
2025-02-04 19:44:55 -0500

    Started work on vision code

--- 2025-Robot-Code/src/main/java/frc/robot/Constants.java
+++ 2025-Robot-Code/src/main/java/frc/robot/Constants.java
@@ -9,6 +9,13 @@ import java.util.List;
 
 import com.ctre.phoenix6.CANBus;
 
+import edu.wpi.first.math.Matrix;
+import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.numbers.N1;
+import edu.wpi.first.math.numbers.N3;
 import edu.wpi.first.math.util.Units;
 
 /**
@@ -85,6 +92,12 @@ public final class Constants {
   public static class VisionConstants {
     public static String kCamera1Name = "";
     public static String kCamera2Name = "";
+
+    public static final Transform3d kRobotToCam1 =
+      new Transform3d(new Translation3d(0.5, 0.0, 0.5), new Rotation3d(0, 0, 0));
+
+    public static final Transform3d kRobotToCam2 =
+      new Transform3d(new Translation3d(0.5, 0.0, 0.5), new Rotation3d(0, 0, 0));
   }
 
   public static class AlgaeConstants {
--- 2025-Robot-Code/src/main/java/frc/robot/subsystems/Vision.java
+++ 2025-Robot-Code/src/main/java/frc/robot/subsystems/Vision.java
@@ -1,10 +1,12 @@
 package frc.robot.subsystems;
 
 import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonPoseEstimator;
+import org.photonvision.PhotonPoseEstimator.PoseStrategy;
 
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
-
+import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.VisionConstants;
 
@@ -12,12 +14,33 @@ public class Vision extends SubsystemBase{
     final AprilTagFieldLayout mFieldLayout;
     final PhotonCamera mCamera1;
     final PhotonCamera mCamera2;
+    final PhotonPoseEstimator poseEstimator;
 
     Vision() {
         mCamera1 = new PhotonCamera(VisionConstants.kCamera1Name);
         mCamera2 = new PhotonCamera(VisionConstants.kCamera1Name);
         mFieldLayout = AprilTagFieldLayout.loadField(AprilTagFields.k2025Reefscape);
+        poseEstimator = new PhotonPoseEstimator(mFieldLayout, PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR, VisionConstants.kRobotToCam1);
+
     }
+
+    /*public Transform3d getCurrentPositionCam1(){
+        var result = mCamera1.getLatestResult();
+        if (result.getMultiTagResult().estimatedPose.isPresent) {
+        Transform3d fieldToCamera = result.getMultiTagResult().estimatedPose.best;
+        }
+    }
+
+    public Transform3d getCurrentPositionCam2(){
+        var result = mCamera1.getLatestResult();
+        if (result.getMultiTagResult().estimatedPose.isPresent) {
+        Transform3d fieldToCamera = result.getMultiTagResult().estimatedPose.best;
+        }
+    }*/
+
+
+
+    
 
 
 
