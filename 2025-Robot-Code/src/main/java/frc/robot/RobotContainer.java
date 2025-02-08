baa62a9d21ae5fb635535abc045f916621d9ad04
demiandk <152564707+demiandk@users.noreply.github.com>
2025-02-08 11:04:23 -0500

    Added elevator testing height values and button configuration

--- 2025-Robot-Code/src/main/java/frc/robot/RobotContainer.java
+++ 2025-Robot-Code/src/main/java/frc/robot/RobotContainer.java
@@ -43,6 +43,7 @@ public class RobotContainer {
     public final CommandSwerveDrivetrain drivetrain = TunerConstants.createDrivetrain();
     public final EndEffector endEffector = new EndEffector();
     public final Vision vision = new Vision();
+    public final Elevator elevator = new Elevator();
 
     private final AutoPicker mAutoPicker = new AutoPicker();
 
@@ -75,10 +76,15 @@ public class RobotContainer {
             )
         );
 
-        driverController.a().whileTrue(drivetrain.applyRequest(() -> brake));
-        driverController.b().whileTrue(drivetrain.applyRequest(() ->
+        driverController.povDown().whileTrue(drivetrain.applyRequest(() -> brake));
+        driverController.povRight().whileTrue(drivetrain.applyRequest(() ->
             point.withModuleDirection(new Rotation2d(-driverController.getLeftY(), -driverController.getLeftX()))
         ));
+
+        driverController.b().whileTrue(elevator.setElevatorTarget(ElevatorConstants.kL1Height)).onFalse(elevator.setElevatorTarget(ElevatorConstants.kStowedHeight));
+        driverController.a().whileTrue(elevator.setElevatorTarget(ElevatorConstants.kL2Height)).onFalse(elevator.setElevatorTarget(ElevatorConstants.kStowedHeight));
+        driverController.x().whileTrue(elevator.setElevatorTarget(ElevatorConstants.kL3Height)).onFalse(elevator.setElevatorTarget(ElevatorConstants.kStowedHeight));
+        driverController.y().whileTrue(elevator.setElevatorTarget(ElevatorConstants.kL4Height)).onFalse(elevator.setElevatorTarget(ElevatorConstants.kStowedHeight));
 
         // Run SysId routines when holding back/start and X/Y.
         // Note that each routine should be run exactly once in a single log.
